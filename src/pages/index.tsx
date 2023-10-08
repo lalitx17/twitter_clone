86e98063530575d3e5b5436e233662f912dbb7aa
lalitx17 <lalityadav.x17@gmail.com>
2023-10-08 18:19:55 -0500

    new features added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,59 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import { SignInButton, SignOutButton, SignUp } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignUp, auth } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
+import type { RouterOutputs } from "~/utils/api";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="border-b border-slate-400 p-4 gap-3 flex">
+      <Image src={author.profilePictureUrl}
+        className="h-16 w-16 rounded-full"
+        height={100}
+        width={100}
+        alt={`@${author.username}`} />
+      <div className="flex flex-col">
+        <div className="flex text-slate-300 gap-1">
+          <span>{`@${author.username}`}</span>
+          <span className="font-thin">{`·  ${dayjs(post.createdAt).fromNow()}`}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
+  );
+};
 
 const CreatePostWizard = () => {
-  const {user} = useUser();
+  const { user } = useUser();
+
+  console.log(user);
 
   if (!user) return null;
 
   return (
     <div className="flex w-full gap-3">
-     <Image
-      src={user.imageUrl}
-      height={100}
-      width={100}
-      alt="Profile Image" 
-      className="h-16 w-16 rounded-full"/>
+      <Image
+        src={user.imageUrl}
+        height={100}
+        width={100}
+        alt="Profile Image"
+        className="h-16 w-16 rounded-full"
+      />
 
-      <input placeholder="Type some emojis!!" className="bg-transparent grow outline-none" />
+      <input
+        placeholder="Type some emojis!!"
+        className="grow bg-transparent outline-none"
+      />
     </div>
   );
 };
@@ -49,10 +83,8 @@ export default function Home() {
             {user.isSignedIn && <CreatePostWizard />}
           </div>
           <div className="flex flex-col">
-            {[...data, ...data]?.map((post) => (
-              <div key={post.id} className="border-b border-slate-400 p-8">
-                {post.content}
-              </div>
+            {[...data, ...data]?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </div>
         </div>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,7 @@
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 
 import {
   createTRPCRouter,
@@ -6,12 +9,38 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
-  }),
+const filterUserForClient = (user: User) => {
+  return { id: user.id, username: user.username, profilePictureUrl: user.imageUrl }
+}
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })).map(filterUserForClient);
+
+
+
+    return posts.map((post) => {   //create an object that combines the post and the data of the author from the clerk. 
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author?.username)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for the post not found",
+        });
+
+      return {
+        post,
+        author:{
+          ...author, 
+          username: author.username
+        }
+      };
+    });
   }),
 });
